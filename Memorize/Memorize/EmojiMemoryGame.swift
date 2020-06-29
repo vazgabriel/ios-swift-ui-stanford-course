ffc01a59c2bd68dd2b4a80161ed3a7a740f87484
vazgabriel <gabrielcvaz@outlook.com>
2020-06-29 18:14:35 -0500

    Add Game, Model and ViewModel

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Gabriel de Carvalho on 6/29/20.
+//  Copyright © 2020 Gabriel de Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { i in emojis[i] }
+    }
+
+    // MARK: - Access to the Model
+
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
